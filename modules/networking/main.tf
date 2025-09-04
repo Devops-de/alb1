67fb98a2e52e37ecc7b198f025ccbc63f23c8f5c
pankaj rai <raipankajrai74@gmail.com>
2025-09-04 19:37:28 +0530

    file missing

---
+++ modules/networking/main.tf
@@ -0,0 +1,30 @@
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = var.azs[count.index]
+  tags = { Name = "public-${count.index}" }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "alb-sg" }
+}
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,3 +1,4 @@
 variable "vpc_id" {}
 variable "public_subnet_cidrs" { type = list(string) }
 variable "azs" { type = list(string) }
+variable "vpc_cidr" {}
