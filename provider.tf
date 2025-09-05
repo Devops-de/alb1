883d4c509f34beb8f5ce386abd1826e79282a3ad
pankaj rai <raipankajrai74@gmail.com>
2025-09-05 11:53:52 +0530

    adding backend configuration for state file

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+    backend "s3" {
+      encrypt = true
+      bucket = "aftp-terraform-statefile"
+      dynamodb_table = "terraform-state-lock-table"
+      key = "infra/terraform.tfstate"
+      region = "us-east-1"
+  }
+}
