b37f96e33d34135a7a49cd75b407fc2791825292
Pankaj Rai <34732863+Devops-de@users.noreply.github.com>
2025-09-11 10:03:12 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,18 +1,23 @@
-
 variable "vpc_cidr_block" {
-  default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr_block" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "azs" {
-  default = ["us-east-1a", "us-east-1b"]
-  
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "acm_cert_arn" {
-    default = "arn:aws:acm:us-demo"
+  description = "ARN of the ACM certificate for HTTPS listener"
+  type        = string
+  default     = ""
 }
-
