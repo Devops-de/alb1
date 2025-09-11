7b11bdfecafcc9ab82b66b61bd65e53e0895c664
Pankaj Rai <34732863+Devops-de@users.noreply.github.com>
2025-09-11 10:05:12 +0530

    Update outputs.tf

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,3 +1,11 @@
+output "alb_arn" {
+  value = aws_lb.app_alb.arn
+}
+
 output "alb_dns_name" {
   value = aws_lb.app_alb.dns_name
+}
+
+output "target_group_arns" {
+  value = { for k, v in aws_lb_target_group.app_tg : k => v.arn }
 }
