7614ab9fc06204ddb57e80a377c80e04b0f72b35
Pankaj Rai <34732863+Devops-de@users.noreply.github.com>
2025-09-11 10:07:06 +0530

    Update variables.tf

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,4 +1,38 @@
-variable "vpc_id" {}
-variable "public_subnet_cidrs" { type = list(string) }
-variable "azs" { type = list(string) }
-variable "vpc_cidr" {}
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "alb_ingress_rules" {
+  description = "List of ingress rules for the ALB security group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
